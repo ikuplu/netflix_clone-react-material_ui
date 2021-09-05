6f35827ed958a5dacfe2afcd4d3fe17bc3b2f976
Ismail Kuplu <i.kuplu@outlook.com>
2021-09-05 14:57:59 +0200

    Added language selector component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Logo } from './Logo';
+import LangSelector from './LangSelector';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,6 +19,7 @@ export const Header = () => {
     <AppBar className={classes.root} position="static">
       <Toolbar>
         <Logo />
+        <LangSelector />
       </Toolbar>
     </AppBar>
   );
---
+++ src/components/LangSelector.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import LanguageIcon from '@material-ui/icons/Language';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    border: '1px solid grey',
+  },
+
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 120,
+    background: 'transparent',
+    display: 'inline-block',
+  },
+
+  langIcon: {
+    marginRight: theme.spacing(0.5),
+    fontSize: '.75rem',
+    color: 'black',
+    display: 'inline-block',
+  },
+}));
+
+export default function LangSelector() {
+  const classes = useStyles();
+  const [lang, setLang] = React.useState('');
+
+  const handleChange = (event) => {
+    const lang = event.target.lang;
+    setLang(lang);
+  };
+
+  return (
+    <div className={classes.root}>
+      <FormControl className={classes.formControl}>
+        <LanguageIcon className={classes.langIcon} color="primary" />
+        <NativeSelect
+          className={classes.nativeSelect}
+          defaultValue="English"
+          inputProps={{
+            lang: 'lang',
+          }}
+        >
+          <option value="Nederlands">Nederlands</option>
+          <option value="English">English</option>
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
+}
